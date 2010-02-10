cd9bdd168b85c01614ffe9329ac79868abcfdfe8
Harald Friessnegger <harald@webmeisterei.com>
2010-02-10 13:05:27 +0000

    sizes can be a callable or a dictionary (instance method not supported)

--- README.txt
+++ README.txt
@@ -4,21 +4,21 @@ Image field for ``Archetypes`` with different behavior at time of scaling.
 Introduction
 ============
 
-Archtypes default ``ImageField`` scales down the image until the whole image 
+Archtypes default ``ImageField`` scales down the image until the whole image
 fits into the given scale. It keeps it aspect ratio. I.e. scaling down a 400x300
 image to a 200x200 scale result in a 200x150 image.
 
-Same with ``ClippingImageField`` results in a 200x200 image! It centers the 
-image horizontal or vertical and tries to keep as much as possible from the 
+Same with ``ClippingImageField`` results in a 200x200 image! It centers the
+image horizontal or vertical and tries to keep as much as possible from the
 original.
 
 ======
 Usage:
 ======
 
-Specify the sizes as documented for the classic Archetypes ImageField. If you 
-want specific scales to not get clipped, add a field-property 
-``classic_crop=['image_large','other_scale']``. It expects a list of scale 
+Specify the sizes as documented for the classic Archetypes ImageField. If you
+want specific scales to not get clipped, add a field-property
+``classic_crop=['image_large','other_scale']``. It expects a list of scale
 names to exclude from clipping
 
 =========
@@ -26,6 +26,16 @@ Copyright
 =========
 
 written by Jens W. Klein, BlueDynamics Alliance, Klein & Partner KEG, Austria
-http://bluedynamics.com 
+http://bluedynamics.com
 
-   
+=========
+Changelog
+=========
+
+after 1.1.1
+-----------
+
+- sizes can be a callable or a dictionary (instance method not supported)
+
+  background: plone.app.imaging uses the sizes defined in imaging_properties in case sizes is a dictionary.
+  so you need to define sizes via a callable to make your custom sizes take effect.
--- src/archetypes/clippingimage/_field.py
+++ src/archetypes/clippingimage/_field.py
@@ -13,44 +13,46 @@ from Products.Archetypes.Field import ImageField
 
 class ClippingImageField(ImageField):
     """Like default ImageField from Archetypes with different scaling behaviour.
-    
-    Scales are clipped centered, the resulting image has almost (+/- 1 pixel) 
-    the scale. 
+
+    Scales are clipped centered, the resulting image has almost (+/- 1 pixel)
+    the scale.
     """
-    
-    _properties = ImageField._properties.copy()    
+
+    _properties = ImageField._properties.copy()
     _properties.update({'classic_crop': []})
-            
-    def crop(self, image, scale):        
+
+    def crop(self, image, scale):
         """Crop given image to scale.
-        
+
         @param image: PIL Image instance
         @param scale: tuple with (width, height)
-        """        
+        """
         cwidth, cheight = image.size
-        cratio = float(cwidth) / float(cheight)    
+        cratio = float(cwidth) / float(cheight)
         twidth, theight = scale
         tratio = float(twidth) / float(theight)
         if cratio > tratio:
-            middlepart = cheight * tratio  
+            middlepart = cheight * tratio
             offset = (cwidth - middlepart) / 2
             box = int(round(offset)), 0, int(round(offset+middlepart)), cheight
-            image = image.crop(box) 
+            image = image.crop(box)
         if cratio < tratio:
             middlepart = cwidth / tratio
             offset = (cheight - middlepart) / 2
             box = 0, int(round(offset)), cwidth, int(round(offset+middlepart))
-            image =  image.crop(box) 
+            image =  image.crop(box)
         return image
 
     # method scale is copied from Products.Archetypes.Field.ImageField.scale
     # see License over there
     def scale(self, data, w, h, default_format = 'PNG'):
         """ scale image"""
-        size = int(w), int(h)    
+        size = int(w), int(h)
         original_file=StringIO(data)
         image = PIL.Image.open(original_file)
-        if size not in [self.sizes[name] for name in self.classic_crop]:
+        #does not work for sizes='inscanteMethod' since we don't have an instance here
+        availableSizes = self.getAvailableSizes(None)
+        if size not in [availableSizes[name] for name in self.classic_crop]:
             image = self.crop(image, size)
         original_mode = image.mode
         if original_mode == '1':
@@ -64,6 +66,5 @@ class ClippingImageField(ImageField):
         thumbnail_file = StringIO()
         image.save(thumbnail_file, format, quality=self.pil_quality)
         thumbnail_file.seek(0)
-        return thumbnail_file, format.lower()    
+        return thumbnail_file, format.lower()
 
-    
